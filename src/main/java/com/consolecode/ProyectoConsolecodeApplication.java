e47c68d23d1ba292bd880907f2966eed308da6d7
Natsu-Efrain <natsusalamander8888@gmail.com>
2021-10-26 23:29:25 -0500

    Prueba

--- src/main/java/com/consolecode/ProyectoConsolecodeApplication.java
+++ src/main/java/com/consolecode/ProyectoConsolecodeApplication.java
@@ -2,7 +2,7 @@ package com.consolecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Prueba
 @SpringBootApplication
 public class ProyectoConsolecodeApplication {
 
